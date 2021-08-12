5fa7faf212966fc7d1e174ca529a75a680fcdad1
Jamie <jameswgriffith@gmail.com>
2021-08-12 17:07:34 -0500

    first commit

---
+++ R/griffithlab_internal_functions.R
@@ -0,0 +1,54 @@
+# griffithlab internal functions ------------------------------------------
+#'
+#' Internal generic function for helping to score surveys
+#'
+#'Takes a dataframe of numeric items and calculates a score
+# by multiplying the number of items by the mean item response.
+# If no items are missing, this is equivalent to summing the items.
+# If one or more items are missing, this is equivalent to treating
+# the missing items as the mean value of the non-missing items,
+# and then summing all of the items. This practice is known as
+# "prorating". A score is only returned if the minimum number
+# of items (or greater) is present.
+#
+# Arguments are:
+# items  - A dataframe containing the responses.
+# Note: If items is not a dataframe, the function will
+# stop and return an error message.
+#
+# min_num_items - The minimum number of items for scoring the case.
+# If the length of items is less than this value, then NA will be returned.
+# By default, i.e., if the user does not supply min_num_items, then
+# all of the items will be required in order to calculate a score.
+#'
+#' @param items A dataframe of numeric  questionnaire items.
+#' @param min_num_items The minimum number of items needed to score the
+#' questionnaire. If not enough items are present, the score will be NA.
+#' If some items are missing, but the number of non-missing items is
+#' min_num_items or higher, then the score will be prorated.
+#'
+#' @return Scores for each case (i.e., row)
+#'
+#' @examples
+#' # score_surveys(rrs, 10)
+#' # score_surveys(rrs, 8)
+#' @keywords internal
+score_surveys <- function (items, min_num_items = ncol(items)) {
+  # Handle some possible errors
+  if (!is.data.frame(items)) {
+    stop("This function only handles dataframes of numeric survey data.\n Try again with items as a dataframe.")
+  }
+  if (min_num_items > ncol(items)) {
+    stop("The argument min_num_items is larger than the number of items.")
+  }
+  if (min_num_items <= 0) {
+    stop("The argument min_num_items should not be zero or negative.")
+  }
+  # Done handling errors, so apply scoring algorithm below.
+  apply(X = items,
+        MARGIN = 1,
+        FUN = function(one_survey)
+          ifelse(test = sum(!is.na(one_survey)) >= min_num_items,
+                 yes = length(one_survey) * mean(one_survey, na.rm = TRUE),
+                 no = NA))
+}
---
+++ R/score_sus.R
@@ -0,0 +1,91 @@
+#' Scores the System Usability Scale (SUS; all 10 items)
+#' 
+#' @description Scores the 10-item System Usability Scale (SUS)
+#' The ten items are 1, 2, 3, 4, 5 coded in the following way:\cr
+#' 1 = Strongly Disagree\cr
+#' 2 = Disagree\cr
+#' 3 = Neutral\cr
+#' 4 = Agree\cr
+#' 5 = Strongly Agree\cr
+#'
+#' @details
+#' In some versions of the SUS, only 1 (Strongly Disagree) and 5 (Strongly Agree) are labelled.
+#'
+#' @param sus_items A matrix (or an object coercible to a matrix) that contains
+#' the items of the System Usability Scale (SUS), with each item
+#' represented as a number, 1, 2, 3, 4, or 5.
+#' @param min_num_items The minimum number of items needed to be non-missing
+#' in order for a score to be given. If the number of non-missing items is
+#' less than min_num_items, then the score will be NA. Otherwise, in the
+#' presence of missing data, prorating will be used. With prorating the score
+#' is (10 * mean item response) The default for the SUS is min_num_items = 10.
+#'
+#' @return Scores for the SUS.
+#' @export
+#'
+#' @examples
+#'\dontrun{
+#' sus_items <- paste0("sus", 1:10)
+#' score_sus(some_data[sus_items])
+#' }
+
+score_sus <- function(sus_items,
+                      min_num_items = 10) {
+  
+  sus_range <- 1:5L
+  
+  n_sus_items <- 10
+  
+  if(ncol(sus_items) != n_sus_items) {
+    stop("The SUS has",
+         n_sus_items,
+         "items, so there should be",
+         n_sus_items, "columns in sus_items.")
+  }
+  
+  if(min_num_items > n_sus_items) {
+    stop("The SUS has",
+         n_sus_items,
+         "items, so min_num_items must be",
+         n_sus_items,
+         "or smaller.")
+  }
+  
+  if(min_num_items < 1) {
+    stop("min_num_items must be greater than 0.")
+  }
+  
+  sus_items <- as.matrix(sus_items)
+  
+  sus_items[which(!sus_items %in% rrs_range,
+                  arr.ind = TRUE)] <- NA
+  
+  if(all(is.na(sus_items))) {
+    message("All items are missing in sus_items.\n")
+    message("Check your input.\n")
+  } else if (any(is.na(sus_items))) {
+    message("Some items are missing in sus_items.\n")
+  }
+  
+  if(min_num_items < n_sus_items && !all(is.na(sus_items))) {
+    message("Scoring will use prorating if some items are missing.\n")
+    message(paste("If you do not want to prorate scores, set min_num_items to",
+                  n_sus_items))
+  }
+ 
+  # For items 1,3,5,7,and 9 the score contribution is the scale position minus 1.
+  # For items 2,4,6,8 and 10, the contribution is 5 minus the scale position.
+  # Multiply the sum of the scores by 2.5 to obtain the overall value of SU.
+  
+  recoded_sus <- matrix(nrow = nrow(sus_items),
+                        ncol = ncol(sus_items))
+  
+  recoded_sus[, c(1, 3, 5, 7, 9)] <- sus_items[, c(1, 3, 5, 7, 9)] - 1
+  recoded_sus[, c(2, 4, 6, 8, 10)] <- 5 - sus_items[, c(2, 4, 6, 8, 10)]
+   
+  recoded_sus <- as.data.frame(recoded_sus)
+  
+  # Return output
+  2.5 * score_surveys(recoded_sus, min_num_items)
+  
+}
